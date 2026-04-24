15ae43d77e51a78b3d9353acfb1bdf7863fec8e5
euclidiano <luisrobertogouvea@gmail.com>
2026-04-24 00:53:39 -0300

    primeiro

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+window.onload = function() {
+    let button = document.getElementById("calcular");
+    button.addEventListener("click", calcularOdio);
+
+
+}
+
+function calcularOdio(){
+    let seuNome = document.getElementById("seu-nome").value;//puxa id do html
+    let Pessoa = document.getElementById("pessoa").value;
+
+    if (seuNome != "" && pessoa != "") {
+        let percentage = Math.floor(Math.random() * 101); //0-.999999 * 101 > 0- 100.999999
+        document.getElementById("resultado").innerText = " A chance de se odiar de " + seuNome + " e " + Pessoa + " é de: "
+        document.getElementById("porcentagem").innerText = percentage.toString() + "%"
+    }
+}
